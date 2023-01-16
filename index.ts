5dc47b0ca4045d56c582692db198de814ddc341f
dcauwe <dorian8080@gmail.com>
2023-01-16 15:58:07 -0700

    Add better timing

--- index.ts
+++ index.ts
@@ -21,17 +21,24 @@ const onClientConnected = (sock: any) => {
             sock.write(currentImageBuffer);
         } else if (data.includes("get_sleep_duration")) {
             const now = new Date();
+            const nextDate = new Date();
             
-            const tomorrow = new Date();
-            tomorrow.setDate(tomorrow.getDate()+1);
-            tomorrow.setHours(3, 0, 0, 0);
+            if (now.getHours() >= 18) {
+                nextDate.setDate(nextDate.getDate()+1);
+                nextDate.setHours(3, 0, 0, 0);
+            } else if (now.getHours() >= 14) {
+                nextDate.setHours(19, 0, 0, 0);
+            } else if (now.getHours() >= 10) {
+                nextDate.setHours(15, 0, 0, 0);
+            } else {
+                nextDate.setHours(10, 0, 0, 0);
+            }
 
-            console.log(tomorrow, "--", now);
+            console.log(nextDate, "--", now);
 
             // Sleep in milliseconds
-            let sleep = dateDiffInMs(tomorrow, now);
+            let sleep = dateDiffInMs(nextDate, now);
             console.log(`Sleep for ${sleep} milliseconds`);
-            sleep = 20*1000;
 
             // Force to 64 bits
             var sleepUs = new Int64BE(sleep * 1000);
